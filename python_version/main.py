703582c5d9273f717f50c574af45e4e8e495a95c
waltertaya <waltermittybecker5@gmail.com>
2023-12-08 14:48:05 +0300

    updated

--- python_version/main.py
+++ python_version/main.py
@@ -2,20 +2,25 @@ import json
 import os
 from llamaapi import LlamaAPI
 
+
 # Initialize the llamaapi with your api_token
-llama = LlamaAPI("API_TOKEN")
+llama = LlamaAPI("api_token")
 
 
 # Define your API request
 def finalTerminalCommand():
     Query = input("Enter your query: ")
-    Query = Query + ". Platform:"+ os.name
-    
+    Query = Query + ". Platform:" + os.name
+
     api_request_json = {
                 "messages": [
                     {
-                        "role" : "system",
-                        "content" : "You are a computer expert. You want to respond to prompts with the commands required to complete the task. You are given a prompt with a task. You must respond with the command to complete the task."
+                        "role": "system",
+                        "content": """You are a computer expert. You want to
+                        respond to prompts with the commands required to
+                        complete the task. You are given a prompt with a task.
+                        You must respond with the command to complete the task.
+                        """
                     },
                     {
                         "role": "user",
@@ -25,24 +30,35 @@ def finalTerminalCommand():
                 "functions": [
                     {
                         "name": "find_terminal_command",
-                        "description": "Get the command to execute a terminal function. Return the command and any other arguments that may be needed to fulfil the command.",
+                        "description": """Get the command to execute a terminal
+                        function. Return the command and any other arguments
+                        that may be needed to fulfil the command.""",
                         "parameters": {
                             "type": "object",
                             "properties": {
                                 "action": {
                                     "title": "action",
                                     "type": "string",
-                                    "description": "The name of the command to execute on the platform, including any flags if necessary"
+                                    "description": """The name of the command
+                                    to execute on the platform, including any
+                                    flags if necessary"""
                                 },
                                 "target": {
                                     "title": "target",
                                     "type": "string",
-                                    "description": "Any required parameters for the action to be performed successfully, and according to the prompt"
+                                    "description": """Any required parameters
+                                    for the action to be performed
+                                    successfully, and according to the prompt
+                                    """
                                 },
                                 "platform": {
                                     "title": "platform",
                                     "type": "string",
-                                    "description": "The platform that is used to determine the command, e.g. Windows , mac , Linux , or the operating system that the command runs on. It should be found from the query."
+                                    "description": """The platform that is used
+                                    to determine the command, e.g. Windows
+                                    , mac , Linux ,or the operating system
+                                    that the command runs on. It should be
+                                    found from the query."""
                                 }
                             }
                         },
@@ -55,21 +71,21 @@ def finalTerminalCommand():
                 ],
                 "temperature": 1.0,
                 "stream": False,
-                "function_call": {"name" : "find_terminal_command"}
+                "function_call": {"name": "find_terminal_command"}
             }
 
     # Make your request and handle the response
     response = llama.run(api_request_json)
     print(json.dumps(response.json(), indent=2))
-    args = response.json()["choices"][0]["message"]["function_call"]["arguments"]
-    return args
+    args = response.json()["choices"][0]["message"]["function_call"]
+    return args["arguments"]
 
 
 while True:
     command = finalTerminalCommand()
 
     # Execute the command
-    if command["target"] not in command:
-        os.system(command["action"])
-    else:
+    if command["target"] != "":
         os.system(command["action"] + " " + command["target"])
+    else:
+        os.system(command["action"])
