ae8c3c890f70050f852a72a9b317d252879c6131
Clint171 <clintsimiyu004@gmail.com>
2023-12-12 20:02:11 +0300

    chore: refactored prompts

--- python_version/v2.py
+++ python_version/v2.py
@@ -20,17 +20,20 @@ messages = [
         "role": "system",
         "content": (
             "You are a translator between natural language and terminal commands. "
-            "You are communicating with a terminal that only understands and "
-            "executes commands."
-            "You receive a prompt with a task. You must respond with the commands."
+            "Your goal is to translate natural language into terminal commands."
+            "You receive a prompt with a task. You must respond with the commands only."
             " You can ONLY respond with a string of commands "
             "in sequence that are ready to be executed as is. You will respond "
             "with all the commands required to achieve the goal given. Do not "
             "number the commands, as the terminal will throw an error. If a path "
-            "is not specified, always use the current directory. You must not not"
-            " explain "
-            "anything, as the terminal does not understand anything other than "
-            "commands."
+            "is not specified, always use the current directory."
+            "Do not explain "
+            "anything, as the terminal does not understand natural language. "
+            "Do not use any punctuation, as the terminal will throw an error. "
+            "Do not use any quotes, as the terminal will throw an error. "
+            "Do not use any backticks, as the terminal will throw an error. "
+            "You cannot change directories, so commands that require changing "
+            "directories will need to be modified to use relative paths. "
         ),
     }
 ]
@@ -49,7 +52,7 @@ def handle_prompt(query):
     # Prepare API request
     api_request = {
         "messages": messages,
-        "temperature": 0.3,
+        "temperature": 0.9,
         "stream": False,
     }
 
@@ -72,7 +75,7 @@ def handle_prompt(query):
                 # remove last message
                 messages.pop()
                 # send error message to llama
-                handle_prompt(command + " failed with the error message : " + stderr.decode('utf-8'))
+                handle_prompt(command + " failed with the error message : " + stderr.decode('utf-8') + ". Retry from this point.")
                 break
             else:
                 print(f"Output:\n{stdout.decode('utf-8')}")
